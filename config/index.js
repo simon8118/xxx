9f5389db1914004cdbb3ca07fd4b10194fe57375
ss <xx@a.com>
2019-10-21 15:43:13 +0800

    跨域 依赖

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: { // 跨域
+      "/api": {
+        target: "http://10.10.21.35:3000",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
